6de79a8a17837b3ebf9e5885f2d9d512d8d56e5b
Em Urdaneta <em.urdaneta94@gmail.com>
2020-09-15 00:22:33 +0000

    create number 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,15 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+
+store3 = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
+store4 = Store.create(name: "Whistler", annual_revenue: 1900000, womens_apparel:false, mens_apparel: true)
+store5 = Store.create(name: "Yaletown", annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
+
+@store_selling_mens_apparel = Store.where(mens_apparel:true).load;
+
+@store_selling_mens_apparel.each do |record|
+  puts record.name
+end
+
